a7497ec5e169b1b771c3a5ba060b007ac21202f6
QuynhPham76 <119039441+QuynhPham76@users.noreply.github.com>
2023-02-13 14:17:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    titleWeb: "quynh'skhai",
+    introTitle: 'Đức Khải',
+    introDesc: `Chào mừng anh đến với Thế Giới Valentine 2023 ngập tràn iu thương cùng bé Nhão`,
+    btnIntro: 'Nhấn vào đây để nhận iu thương nè',
+    title: 'Khoannnn... Anh có thương bé hong 🥰',
+    desc: 'Chỉ được chọn một lần nên suy nghĩ cho kỹ nha ',
+    btnYes: 'Nằm mơ đi',
+    btnNo: 'Có, iu bé nhìu lém <33',
+    question: 'Khoảnh khắc cùng em khiến anh nhớ nhất <3',
+    btnReply: 'Chứ còn sao nữa <3',
+    reply: 'Anh iu tất cả khoảnh khắc cùng em vì em lun chinh đẹp <33333333',
+    mess: 'Em biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://vt.tiktok.com/ZS85htCGH/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
